bf58371f46a5c262bbc511457d36d1065b55e480
David Holmes <dholmes@dholmes.us>
2023-02-05 17:55:09 -0600

    Fixes to make Emscripten work

--- cmake/ConfigureVcpkg.cmake
+++ cmake/ConfigureVcpkg.cmake
@@ -1,3 +1,5 @@
+include(CMakePrintHelpers)
+
 # Configure CMake to use the vcpkg CMake toolchain file, found in the vcpkg submodule
 function(configure_vcpkg VCPKG_TOOLCHAIN_FILE)
   if(NOT EXISTS ${VCPKG_TOOLCHAIN_FILE})
@@ -7,14 +9,19 @@ function(configure_vcpkg VCPKG_TOOLCHAIN_FILE)
     )
   else()
     if(DEFINED CMAKE_TOOLCHAIN_FILE)
-      if(NOT EQUAL ${VCPKG_CHAINLOAD_TOOLCHAIN_FILE} ${CMAKE_TOOLCHAIN_FILE})
+      if(NOT ${VCPKG_TOOLCHAIN_FILE} EQUAL ${CMAKE_TOOLCHAIN_FILE})
+        message(STATUS "Non-vcpkg toolchain file will be chainloaded: ${CMAKE_TOOLCHAIN_FILE}")
         set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE
             ${CMAKE_TOOLCHAIN_FILE}
-            PARENT_SCOPE)
+            CACHE FILEPATH "Former value of CMAKE_TOOLCHAIN_FILE to be chain-loaded by vcpkg toolchain" FORCE)
+      else()
+        message(STATUS "Toolchain is already set to vcpkg toolchain file; chainload file will not be modified.")
       endif()
+    else()
+      message(STATUS "No toolchain file is set, so it will not be chainloaded.")
     endif()
     set(CMAKE_TOOLCHAIN_FILE
         ${VCPKG_TOOLCHAIN_FILE}
-        PARENT_SCOPE)
+        CACHE FILEPATH "CMake toolchain file" FORCE)
   endif()
 endfunction()
